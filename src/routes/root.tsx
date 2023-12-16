106e8525eb3a25da9df0c44c8b3c207c610f5010
Martin Pllu <martin.pllu@wealthwizards.com>
2023-12-16 16:00:24 +0000

    Created

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,25 +1,22 @@
 import { Hono } from "hono";
+import { AppWrapper, Button, ThemeProvider } from "@wealthwizards/design-system";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-export default new Hono().get("/", (c) =>
-    c.html(
-        <html>
-            <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>My app</title>
-                <script src="https://unpkg.com/htmx.org@1.9.6"></script>
-                <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet" />
-                <link href="/index.css" rel="stylesheet" />
-            </head>
-            <body>
-                <h1>Daily fluctuations</h1>
-                <div
-                    class="spinner"
-                    style={{ width: "400px", height: "300px" }}
-                    hx-get="/my-chart"
-                    hx-trigger="load"
-                    hx-swap="outerHTML"
-                ></div>
-            </body>
-        </html>
-    )
-);
+const TENANT = "gilbert";
+// const TENANT = "ruby";
+
+const MyComponent = () => {
+    return (
+        <ThemeProvider tenant={TENANT}>
+            <AppWrapper>
+                <h1>Hello world</h1>
+                <Button>Hi</Button>
+            </AppWrapper>
+        </ThemeProvider>
+    );
+};
+
+const renderedString = ReactDOMServer.renderToString(<MyComponent />);
+
+export default new Hono().get("/", (c) => c.html(renderedString));
