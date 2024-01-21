14f7acb9b1d27ebf8e234143ad5b9c5de0b0caa3
Martin Pllu <martin.pllu@wealthwizards.com>
2024-01-21 08:41:39 +0000

    page function

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -9,68 +9,81 @@ const app = new Hono();
 export default app;
 
 app.get("/", async (c) => {
-    const errors = c.req.query("errors")?.split(",") || [];
-    return c.html(
-        renderToString(
-            <ThemeProvider tenant="turo">
-                <Page>
-                    <AppWrapper>
-                        <Page>
-                            <HeroBox>
-                                <HeroBox.Main>
-                                    <Heading type="h1" size="xl" color={"primaryBrand"} css={headingCss}>
-                                        Find out your retirement income in 5 minutes
-                                    </Heading>
-                                    <Text size={"xl"} color={"primaryBrand"}>
-                                        Answer a few easy questions and find out your retirement income options
-                                    </Text>
-                                    <Form action="/" method="post" hx-post="/" hx-target="#main" hx-swap="innerHTML">
-                                        <Form.InputSet>
-                                            <div style={{ display: "flex" }}>
-                                                <Input>
-                                                    <Input.Label>Start by telling us your age&nbsp;</Input.Label>
-                                                    <Input.Select name="age">
-                                                        {[45, 50, 60].map((i) => (
-                                                            <option value={i} key={"age-" + i}>
-                                                                {i}
-                                                            </option>
-                                                        ))}
-                                                    </Input.Select>
-                                                    {errors.includes("age") && <Input.Error>Please select your age</Input.Error>}
-                                                </Input>
-                                            </div>
-                                            <Input>
-                                                <Input.LabelSpacer />
-                                                <Button type={"submit"}>Next</Button>
-                                            </Input>
-                                        </Form.InputSet>
-                                    </Form>
-                                </HeroBox.Main>
-                                <HeroBox.Aside>
-                                    <TaggedIllustration illustration="PiggyBank">
-                                        <TaggedIllustration.Tag $offset={5}>"When can I retire?"</TaggedIllustration.Tag>
-                                        <TaggedIllustration.Tag $offset={45}>"Should I work part-time?"</TaggedIllustration.Tag>
-                                        <TaggedIllustration.Tag>"Holiday or a new car?"</TaggedIllustration.Tag>
-                                    </TaggedIllustration>
-                                </HeroBox.Aside>
-                            </HeroBox>
-                        </Page>
-                    </AppWrapper>
-                </Page>
-            </ThemeProvider>
-        )
-    );
+    return c.html(page({}));
 });
 
 app.post("/", async (c) => {
     const body = await c.req.parseBody();
     if (!body.age) {
-        return c.redirect("?errors=age");
+        return c.html(page({ missingAgeError: true }));
     } else {
+        // save to database here...
         return c.redirect("your-details");
     }
-    // save to database here...
 });
+
+interface State {
+    missingAgeError?: boolean;
+}
+
+function page(state: State) {
+    return renderToString(
+        <ThemeProvider tenant="turo">
+            <Page>
+                <AppWrapper>
+                    <Page>
+                        <HeroBox>
+                            <HeroBox.Main>
+                                <Heading type="h1" size="xl" color={"primaryBrand"} css={headingCss}>
+                                    Find out your retirement income in 5 minutes
+                                </Heading>
+                                <Text size={"xl"} color={"primaryBrand"}>
+                                    Answer a few easy questions and find out your retirement income options
+                                </Text>
+                                <Form action="/" method="post" hx-post="/" hx-target="#main" hx-swap="innerHTML" hx-push-url="true">
+                                    <Form.InputSet>
+                                        <div style={{ display: "flex" }}>
+                                            <Input>
+                                                <Input.Label>Start by telling us your age&nbsp;</Input.Label>
+                                                <Input.Select name="age">
+                                                    {range(40, 80).map((i) => (
+                                                        <option value={i} key={"age-" + i}>
+                                                            {i}
+                                                        </option>
+                                                    ))}
+                                                </Input.Select>
+                                                {state.missingAgeError && <Input.Error>Please select your age</Input.Error>}
+                                            </Input>
+                                        </div>
+                                        <Input>
+                                            <Input.LabelSpacer />
+                                            <Button type={"submit"}>Next</Button>
+                                        </Input>
+                                    </Form.InputSet>
+                                </Form>
+                            </HeroBox.Main>
+                            <HeroBox.Aside>
+                                <TaggedIllustration illustration="PiggyBank">
+                                    <TaggedIllustration.Tag $offset={5}>"When can I retire?"</TaggedIllustration.Tag>
+                                    <TaggedIllustration.Tag $offset={45}>"Should I work part-time?"</TaggedIllustration.Tag>
+                                    <TaggedIllustration.Tag>"Holiday or a new car?"</TaggedIllustration.Tag>
+                                </TaggedIllustration>
+                            </HeroBox.Aside>
+                        </HeroBox>
+                    </Page>
+                </AppWrapper>
+            </Page>
+        </ThemeProvider>
+    );
+}
+
+function range(start: number, end: number) {
+    const result = [];
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    }
+    return result;
+}
 
 const headingCss: Interpolation<Theme> = ({ spacing }) => css`
     margin-top: ${spacing.s24};
--- src/pages/your-details.page.tsx
+++ src/pages/your-details.page.tsx
@@ -7,37 +7,39 @@ const app = new Hono();
 export default app;
 
 app.get("/", (c) => {
-    return c.html(
-        renderToString(
-            <ThemeProvider tenant="turo">
-                <AppWrapper>
-                    <Heading type="h1" size="l" id="page-heading">
-                        What's brought you here today?
-                    </Heading>
-                    <Form.FormItem>
-                        <ChipGroup value="hi" onChange={() => {}}>
-                            <ChipGroup.Legend>Select the one that matters to you most:</ChipGroup.Legend>
-                            <ChipGroup.Chip value="v1" key="k1">
-                                <Chip.Label illustration={"Calendar"}>Find out if I can afford to retire early</Chip.Label>
-                            </ChipGroup.Chip>
-                            <ChipGroup.Chip value="v2" key="k2">
-                                <Chip.Label illustration={"DocumentsQuestion"}>Understand my retirement income</Chip.Label>
-                            </ChipGroup.Chip>
-                            <ChipGroup.Chip value="v3" key="k3">
-                                <Chip.Label illustration={"BalanceScales"}>I'm not sure where to start</Chip.Label>
-                            </ChipGroup.Chip>
-                            <ChipGroup.Chip value="v4" key="k4">
-                                <Chip.Label illustration={"SavingIncrease"}>I'm retired and want some help with my plans</Chip.Label>
-                            </ChipGroup.Chip>
-                            {/* {isError && <ChipGroup.Error>{validationError}</ChipGroup.Error>} */}
-                        </ChipGroup>
-                    </Form.FormItem>
-                </AppWrapper>
-            </ThemeProvider>
-        )
-    );
+    return c.html(page());
 });
 
 app.post("/", async (c) => {
     // ...
 });
+
+function page() {
+    return renderToString(
+        <ThemeProvider tenant="turo">
+            <AppWrapper>
+                <Heading type="h1" size="l" id="page-heading">
+                    What's brought you here today?
+                </Heading>
+                <Form.FormItem>
+                    <ChipGroup value="hi" onChange={() => {}}>
+                        <ChipGroup.Legend>Select the one that matters to you most:</ChipGroup.Legend>
+                        <ChipGroup.Chip value="v1" key="k1">
+                            <Chip.Label illustration={"Calendar"}>Find out if I can afford to retire early</Chip.Label>
+                        </ChipGroup.Chip>
+                        <ChipGroup.Chip value="v2" key="k2">
+                            <Chip.Label illustration={"DocumentsQuestion"}>Understand my retirement income</Chip.Label>
+                        </ChipGroup.Chip>
+                        <ChipGroup.Chip value="v3" key="k3">
+                            <Chip.Label illustration={"BalanceScales"}>I'm not sure where to start</Chip.Label>
+                        </ChipGroup.Chip>
+                        <ChipGroup.Chip value="v4" key="k4">
+                            <Chip.Label illustration={"SavingIncrease"}>I'm retired and want some help with my plans</Chip.Label>
+                        </ChipGroup.Chip>
+                        {/* {isError && <ChipGroup.Error>{validationError}</ChipGroup.Error>} */}
+                    </ChipGroup>
+                </Form.FormItem>
+            </AppWrapper>
+        </ThemeProvider>
+    );
+}
