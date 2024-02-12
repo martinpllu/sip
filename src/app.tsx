1ed0a023d3a23ce9d7dd645a80bd54ba99d084a2
Martin Pllu <martin.pllu@wealthwizards.com>
2024-02-12 14:48:19 +0000

    TodoMVC implementation

---
+++ src/app.tsx
@@ -0,0 +1,85 @@
+import { Hono } from "hono";
+import { v4 as uuidv4 } from "uuid";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const todos = [];
+
+function renderTodos() {
+    return (
+        <div id="main">
+            <h1>TodoMVC</h1>
+            <form hx-post="/add-todo" hx-target="#main" hx-swap="outerHTML">
+                <input type="text" name="text" placeholder="Add a new todo" required autofocus />
+                <button type="submit">Add</button>
+            </form>
+            <div>
+                {todos.map((todo) => (
+                    <div class="todo-item" id={todo.id}>
+                        <input
+                            type="checkbox"
+                            class="todo-complete"
+                            hx-post={`/toggle-todo?id=${todo.id}`}
+                            hx-target="#main"
+                            hx-swap="outerHTML"
+                            value={todo.id}
+                            checked={todo.completed ? true : false}
+                        />
+                        <span class={`todo-text ${todo.completed ? "todo-completed" : ""}`}>{todo.text}</span>
+                        <button
+                            class="todo-delete"
+                            hx-post={`/delete-todo?id=${todo.id}`}
+                            hx-target="#main"
+                            hx-swap="outerHTML"
+                            value={todo.id}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export const app = new Hono();
+app.use("/assets/*", serveStatic({ root: "./" }));
+
+app.get("/", (c) =>
+    c.html(
+        <html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>TodoMVC</title>
+                <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+                <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet" />
+                <link href="/assets/index.css" rel="stylesheet" />
+            </head>
+            <body>{renderTodos()}</body>
+        </html>
+    )
+);
+
+app.post("/add-todo", async (c) => {
+    const { text } = await c.req.parseBody();
+    const newTodo = { id: uuidv4(), text, completed: false };
+    todos.push(newTodo);
+    return c.html(renderTodos());
+});
+
+app.post("/toggle-todo", async (c) => {
+    const id = c.req.query("id");
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+        todo.completed = !todo.completed;
+    }
+    return c.html(renderTodos());
+});
+
+app.post("/delete-todo", async (c) => {
+    const id = c.req.query("id");
+    const index = todos.findIndex((todo) => todo.id === id);
+    if (index > -1) {
+        todos.splice(index, 1);
+    }
+    return c.html(renderTodos());
+});
--- src/handler.tsx
+++ src/handler.tsx
@@ -1,15 +1,4 @@
-import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
-import myChart from "./routes/my-chart";
-import root from "./routes/root";
-import { initCharts } from "./util/echarts";
-import { readFileSync } from "fs";
-
-initCharts();
-const app = new Hono();
-
-app.route("/", root);
-app.route("/my-chart", myChart);
-app.get("/index.css", (c) => c.text(readFileSync("assets/index.css", "utf-8")));
+import { app } from "./app";
 
 export const handler = handle(app);
