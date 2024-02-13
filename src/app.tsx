4d53dcf1182b3bb8bc6463ceb138aaf0e6e56423
Martin Pllu <martin.pllu@wealthwizards.com>
2024-02-13 16:16:46 +0000

    Tidy up

--- src/app.tsx
+++ src/app.tsx
@@ -3,60 +3,7 @@ import { Hono } from "hono";
 import { deleteCookie } from "hono/cookie";
 import { useSession } from "sst/node/auth";
 import { v4 as uuidv4 } from "uuid";
-import { createTodo, deleteTodo, listTodos, toggleTodo } from "./services/todo-service";
-
-async function renderTodos() {
-    const session = useSession();
-    if (session.type !== "user") {
-        return (
-            <div class="login-message">
-                <a href="#" onclick="window.location.href = '/auth/google/authorize'">
-                    Log in with Google
-                </a>
-            </div>
-        );
-    } else {
-        const todos = await listTodos(session.properties.userId);
-        return (
-            <>
-                <div class="login-message">
-                    Welcome {session.properties.firstName}&nbsp;
-                    <a href="/logout">Log out</a>
-                </div>
-
-                <form hx-post="/add-todo" hx-target="#main" hx-swap="innerHTML">
-                    <input type="text" name="text" placeholder="Add a new todo" required autofocus />
-                    <button type="submit">Add</button>
-                </form>
-                <div>
-                    {todos.map((todo) => (
-                        <div class="todo-item" id={todo.id}>
-                            <input
-                                type="checkbox"
-                                class="todo-complete"
-                                hx-post={`/toggle-todo?id=${todo.id}`}
-                                hx-target="#main"
-                                hx-swap="innerHTML"
-                                value={todo.id}
-                                checked={todo.completed ? true : false}
-                            />
-                            <span class={`todo-text ${todo.completed ? "todo-completed" : ""}`}>{todo.text}</span>
-                            <button
-                                class="todo-delete"
-                                hx-post={`/delete-todo?id=${todo.id}`}
-                                hx-target="#main"
-                                hx-swap="innerHTML"
-                                value={todo.id}
-                            >
-                                Delete
-                            </button>
-                        </div>
-                    ))}
-                </div>
-            </>
-        );
-    }
-}
+import { TodoService } from "./services/todo-service";
 
 export const app = new Hono();
 app.use("/assets/*", serveStatic({ root: "./" }));
@@ -66,14 +13,14 @@ app.get("/", (c) =>
         <html>
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>TodoMVC</title>
+                <title>TodoMVC!</title>
                 <script src="https://unpkg.com/htmx.org@1.9.6"></script>
                 <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet" />
                 <link href="/assets/index.css" rel="stylesheet" />
             </head>
             <body>
                 <h1>TodoMVC</h1>
-                <div id="main">{renderTodos()}</div>
+                <div id="main">{useSession().type === "user" ? showTodos() : promptLogin()}</div>
             </body>
         </html>
     )
@@ -83,8 +30,8 @@ app.post("/add-todo", async (c) => {
     const { text } = await c.req.parseBody();
     const session = useSession();
     if (session.type === "user") {
-        await createTodo({ id: uuidv4(), text: text as string, completed: false, user_id: session.properties.userId });
-        return c.html(renderTodos());
+        await TodoService.create({ id: uuidv4(), text: text as string, completed: false, user_id: session.properties.userId });
+        return c.html(showTodos());
     } else {
         return c.status(401);
     }
@@ -94,8 +41,8 @@ app.post("/toggle-todo", async (c) => {
     const session = useSession();
     if (session.type === "user") {
         const id = c.req.query("id");
-        await toggleTodo(session.properties.userId, id);
-        return c.html(renderTodos());
+        await TodoService.toggle(session.properties.userId, id);
+        return c.html(showTodos());
     } else {
         return c.status(401);
     }
@@ -105,8 +52,8 @@ app.post("/delete-todo", async (c) => {
     const session = useSession();
     if (session.type === "user") {
         const id = c.req.query("id");
-        await deleteTodo(session.properties.userId, id);
-        return c.html(renderTodos());
+        await TodoService.delete(session.properties.userId, id);
+        return c.html(showTodos());
     } else {
         return c.status(401);
     }
@@ -116,3 +63,59 @@ app.get("/logout", async (c) => {
     deleteCookie(c, "auth-token");
     return c.redirect("/");
 });
+
+async function promptLogin() {
+    return (
+        <div class="login-message">
+            <a href="#" onclick="window.location.href = '/auth/google/authorize'">
+                Log in with Google
+            </a>
+        </div>
+    );
+}
+
+async function showTodos() {
+    const session = useSession();
+    if (session.type !== "user") {
+        return <div />;
+    }
+    const todos = await TodoService.list(session.properties.userId);
+    return (
+        <>
+            <div class="login-message">
+                Welcome {session.properties.firstName}&nbsp;
+                <a href="/logout">Log out</a>
+            </div>
+
+            <form hx-post="/add-todo" hx-target="#main" hx-swap="innerHTML">
+                <input type="text" name="text" placeholder="Add a new todo" required autofocus />
+                <button type="submit">Add</button>
+            </form>
+            <div>
+                {todos.map((todo) => (
+                    <div class="todo-item" id={todo.id}>
+                        <input
+                            type="checkbox"
+                            class="todo-complete"
+                            hx-post={`/toggle-todo?id=${todo.id}`}
+                            hx-target="#main"
+                            hx-swap="innerHTML"
+                            value={todo.id}
+                            checked={todo.completed ? true : false}
+                        />
+                        <span class={`todo-text ${todo.completed ? "todo-completed" : ""}`}>{todo.text}</span>
+                        <button
+                            class="todo-delete"
+                            hx-post={`/delete-todo?id=${todo.id}`}
+                            hx-target="#main"
+                            hx-swap="innerHTML"
+                            value={todo.id}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
--- src/services/todo-service.ts
+++ src/services/todo-service.ts
@@ -1,23 +1,22 @@
-import { eq, sql } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 import { db } from "../db/db";
 import { todos } from "../db/schema";
 import { Todo } from "./todo";
 
-export async function createTodo(todo: Todo) {
-    await db.insert(todos).values(todo);
-}
-
-export async function listTodos(user: string): Promise<Todo[]> {
-    return await db
-        .select()
-        .from(todos)
-        .where(sql`${todos.user_id} = ${user}`);
-}
-
-export async function toggleTodo(user: string, id: string) {
-    await db.execute(sql`update ${todos} set completed = NOT completed where ${todos.user_id} = ${user} and ${todos.id} = ${id}`);
-}
-
-export async function deleteTodo(user: string, id: string) {
-    await db.execute(sql`delete from ${todos} where ${todos.user_id} = ${user} and ${todos.id} = ${id}`);
-}
+export const TodoService = {
+    create: async function (todo: Todo) {
+        await db.insert(todos).values(todo);
+    },
+    list: async function (user: string): Promise<Todo[]> {
+        return await db
+            .select()
+            .from(todos)
+            .where(sql`${todos.user_id} = ${user}`);
+    },
+    toggle: async function (user: string, id: string) {
+        await db.execute(sql`update ${todos} set completed = NOT completed where ${todos.user_id} = ${user} and ${todos.id} = ${id}`);
+    },
+    delete: async function (user: string, id: string) {
+        await db.execute(sql`delete from ${todos} where ${todos.user_id} = ${user} and ${todos.id} = ${id}`);
+    },
+};
